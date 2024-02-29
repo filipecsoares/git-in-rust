1910fd131fd6de2cf2d693305e4a1d2b8c883134
filipecsoares <filipecarlossoares@gmail.com>
2024-02-28 21:00:58 -0300

    feat: git init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+    if args[1] == "init" {
+        fs::create_dir(".git").expect("Could not create .git folder");
+        fs::create_dir(".git/objects").expect("Could not create .git/objects folder");
+        fs::create_dir(".git/refs").expect("Could not create .git/refs folder");
+        fs::write(".git/HEAD", "ref: refs/heads/master\n").expect("Could not create HEAD file");
+        println!("Initialized git directory");
+    }
 }
