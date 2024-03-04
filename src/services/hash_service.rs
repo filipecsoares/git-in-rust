2991146a2691196c339fc26904a2e78eb311fd7d
filipecsoares <filipecarlossoares@gmail.com>
2024-03-04 14:16:30 -0300

    feat: implement the sha1 hash function

--- src/services/hash_service.rs
+++ src/services/hash_service.rs
@@ -1,4 +1,8 @@
+use crypto::sha1::Sha1;
+use crypto::digest::Digest;
+
 pub fn hash(data: &String) -> String {
-    // TODO - You'll need to implement a hash function
-    data.to_string()
+    let mut hasher = Sha1::new();
+    hasher.input_str(data);
+    hasher.result_str()
 }
