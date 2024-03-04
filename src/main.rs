f05e1019d585ad9cf080f1a00b75aee4d7bfd3ea
filipecsoares <filipecarlossoares@gmail.com>
2024-03-04 08:57:27 -0300

    feat: git add by file

--- src/main.rs
+++ src/main.rs
@@ -19,5 +19,18 @@ fn main() {
         fs::write(".git/config", "[core]\n\trepositoryformatversion = 0\n\tfilemode = true\n\tbare = false\n\tlogallrefupdates = true\n").expect("Could not create config file");
         fs::write(".git/description", "Unnamed repository; edit this file 'description' to name the repository.\n").expect("Could not create description file");
         println!("Initialized git directory");
+    } else if args[1] == "add" {
+        let file_path = &args[2];
+        let data = fs::read_to_string(file_path).expect("Could not read file");
+        let hashed_data = hash(&data);
+        fs::write(format!(".git/objects/{}", hashed_data), data).expect("Could not write object file");
+        println!("Changes to file {} have been staged.", file_path);
+    } else {
+        println!("Invalid command");
     }
+}
+
+fn hash(data: &String) -> String {
+    // TODO - You'll need to implement a hash function
+    data.to_string()
 }
