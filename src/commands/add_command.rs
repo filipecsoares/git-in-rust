986442dc3e136dda4a98b8d873a3993ca0555f09
filipecsoares <filipecarlossoares@gmail.com>
2024-03-05 12:33:35 -0300

    feat: git add .

--- src/commands/add_command.rs
+++ src/commands/add_command.rs
@@ -1,3 +1,5 @@
+use walkdir::WalkDir;
+use std::path::Path;
 use std::fs;
 use crate::command::Command;
 use crate::services::hash_service::hash;
@@ -7,9 +9,21 @@ pub struct Add;
 impl Command for Add {
     fn execute(&self, args: &[String]) {
         let file_path = &args[2];
-        let data = fs::read_to_string(file_path).expect("Could not read file");
-        let hashed_data = hash(&data);
-        fs::write(format!(".git/objects/{}", hashed_data), data).expect("Could not write object file");
-        println!("Changes to file {} have been staged.", file_path);
+        if file_path == "." {
+            for entry in WalkDir::new(".").into_iter().filter_map(|e| e.ok()) {
+                if entry.file_type().is_file() {
+                    stage_file(entry.path());
+                }
+            }
+        } else {
+            stage_file(Path::new(file_path));
+        }
     }
+}
+
+fn stage_file(file_path: &Path) {
+    let data = fs::read_to_string(file_path).expect("Could not read file");
+    let hashed_data = hash(&data);
+    fs::write(format!(".git/objects/{}", hashed_data), data).expect("Could not write object file");
+    println!("Changes to file {:?} have been staged.", file_path);
 }
