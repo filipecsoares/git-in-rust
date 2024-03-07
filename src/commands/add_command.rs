c0fcf8772c65377776c13ae14197353abff9ec80
filipecsoares <filipecarlossoares@gmail.com>
2024-03-07 13:43:11 -0300

    fix: creating directory with first two characters of the hash

--- src/commands/add_command.rs
+++ src/commands/add_command.rs
@@ -24,6 +24,9 @@ impl Command for Add {
 fn stage_file(file_path: &Path) {
     let data = fs::read_to_string(file_path).expect("Could not read file");
     let hashed_data = hash(&data);
-    fs::write(format!(".git/objects/{}", hashed_data), data).expect("Could not write object file");
+    let dir = format!(".git/objects/{}", &hashed_data[..2]);
+    let file = format!("{}/{}", dir, &hashed_data[2..]);
+    fs::create_dir_all(dir).expect("Could not create objects directory");
+    fs::write(file, data).expect("Could not write object file");
     println!("Changes to file {:?} have been staged.", file_path);
 }
